6f7e5522eebff8369f3e2fd87a4c5bf83c019143
kallagoutham <kallagoutham33@gmail.com>
2024-09-24 19:24:10 -0400

    calculator spelling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Calculator from './Components/Caluculator';
+import Calculator from './Components/Calculator';
 
 function App() {
   return (
--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../styles/Caluculator.css";
+import "../styles/Calculator.css";
 
 const Calculator = () => {
   const [input, setInput] = useState("");
